fb05efb2fc3127771317cc6c3f13554d12fedd7a
Your Name <you@example.com>
2025-06-29 19:42:56 +0530

    add

--- StopWatch/src/App.jsx
+++ StopWatch/src/App.jsx
@@ -3,30 +3,26 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  
   let [second, setSecond] = useState(0);
   let [running, setRunning] = useState(false);
 
   useEffect(() => {
     let timer;
-    if(running){
+    if (running) {
       timer = setInterval(() => {
-        setSecond(prev => 
-          prev + 1)
-      
-      }, 1000)
+        setSecond((prev) => prev + 1);
+      }, 1000);
     }
 
-    return() => clearInterval(timer);
-
+    return () => clearInterval(timer);
   }, [running]);
 
-  function formatTime(unit){
+  function formatTime(unit) {
     return unit < 10 ? `0${unit}` : unit;
   }
 
   let minute = Math.floor(second / 60);
-  let remaingSecond = (second % 60);
+  let remaingSecond = second % 60;
 
   let handleClick = () => {
     setRunning(!running);
@@ -36,15 +32,16 @@ function App() {
     setRunning(false);
     setSecond(0);
   };
-   
 
   return (
     <div className="container">
       <h1>Stopwatch</h1>
-     <span style={{fontSize: 'larger', fontWeight: 'bold'}}>Time</span>  : {formatTime(minute)}:{formatTime(remaingSecond)}
+      <span style={{ fontSize: "larger", fontWeight: "bold" }}>
+        Time:
+      </span> {formatTime(minute)}:{formatTime(remaingSecond)}
       <div>
-        <button onClick={handleClick}>{running ? "STOP" : "START"}</button>
-        <button onClick={handleReset}>RESET</button>
+        <button onClick={handleClick}>{running ? "Stop" : "Start"}</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
     </div>
   );
